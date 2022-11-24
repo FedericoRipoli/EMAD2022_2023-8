87924961286ade5238b81c9b5c0030812e56bf04
Tullio Pizzuti <tullio.pizz@gmail.com>
2022-11-24 10:15:05 +0100

    fix type

--- sws_frontend/lib/services/EnteService.dart
+++ sws_frontend/lib/services/EnteService.dart
@@ -1,8 +1,7 @@
 import 'dart:convert';
 
-import 'package:flutter/cupertino.dart';
 import 'package:frontend_sws/services/dto/SignupDTO.dart';
-import 'package:frontend_sws/services/entity/Utente.dart';
+import 'package:frontend_sws/services/entity/Ente.dart';
 import 'package:frontend_sws/services/dto/ListResponse.dart';
 import 'package:logging/logging.dart';
 import 'package:http/http.dart' as http;
@@ -13,7 +12,7 @@ class EnteService {
   final log = Logger('EnteServiceLogger');
   UserService userService = UserService();
 
-  Future<List<Utente>?> enteList(
+  Future<List<Ente>?> enteList(
       String? name,int? page) async {
     try {
       List<String> queryArr=[];
@@ -34,13 +33,13 @@ class EnteService {
           headers: RestURL.defaultHeader);
       if (response.statusCode == 200) {
         if(page!=null){
-          ListResponse<Utente> l = ListResponse<Utente>.fromJson(
-              jsonDecode(response.body), Utente.fromJson);
+          ListResponse<Ente> l = ListResponse<Ente>.fromJson(
+              jsonDecode(response.body), Ente.fromJson);
           return l.content;
         }
         else{
           Iterable l = json.decode(response.body);
-          List<Utente> notPage = List<Utente>.from(l.map((model)=> Utente.fromJson(model)));
+          List<Ente> notPage = List<Ente>.from(l.map((model)=> Ente.fromJson(model)));
           return notPage;
         }
 
