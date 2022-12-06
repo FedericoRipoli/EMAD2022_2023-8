5ab659ad468f3b49bc8cb7f4878d6bdef08e42ef
Tullio Pizzuti <tullio.pizz@gmail.com>
2022-12-06 14:42:35 +0100

    fix query list servizi

--- sws_backend/SWS/src/main/java/it/unisa/emad/comunesalerno/sws/repository/ServizioRepository.java
+++ sws_backend/SWS/src/main/java/it/unisa/emad/comunesalerno/sws/repository/ServizioRepository.java
@@ -4,6 +4,7 @@ import it.unisa.emad.comunesalerno.sws.entity.Servizio;
 import it.unisa.emad.comunesalerno.sws.repository.search.specification.ServizioSpecification;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -12,5 +13,15 @@ import org.springframework.stereotype.Repository;
 
 public interface ServizioRepository extends JpaRepository<Servizio, String>, JpaSpecificationExecutor<Servizio>, ServizioRepositoryCustom {
 
-
+    @EntityGraph(
+            type = EntityGraph.EntityGraphType.FETCH,
+            attributePaths = {
+                    "contatto",
+                    "struttura",
+                    "struttura.posizione",
+                    "aree",
+                    "hashtags"
+            }
+    )
+    Page<Servizio> findAll(Specification sp, Pageable p);
 }
